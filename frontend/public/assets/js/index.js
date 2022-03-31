68e531233a10190531fd8a36cb5985adc4a3f2a9
Miguel Azzopardi <miguelazz2000@gmail.com>
2022-03-31 10:55:08 +0200

    Testing FireStore email 2

--- frontend/public/assets/js/index.js
+++ frontend/public/assets/js/index.js
@@ -1,3 +1,5 @@
+import GetUser from "./db.js"
+
 let signInButton = document.getElementById("signIn");
 let signOutButton = document.getElementById("signOut");
 let profile = document.getElementById("profile");
