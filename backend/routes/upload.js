47fcc5bf8ca6aa7e63fca4d42ad9504693596a6c
MiguelAzzopardi <miguelazz2000@gmail.com>
2022-04-01 11:05:39 +0200

    ticketTesting

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -2,6 +2,7 @@ import Express from "express";
 import multer from "multer";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
+import storage from "@google-cloud/storage";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
