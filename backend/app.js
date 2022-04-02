44d76912ad8d265ff86633cc161303cc31c8587f
Miguel Azzopardi <miguelazz2000@gmail.com>
2022-04-02 17:48:31 +0200

    9

--- backend/app.js
+++ backend/app.js
@@ -84,7 +84,7 @@ app.use("/upload", upload);
 
 //Delivering index.html
 app.get("/", (req, res)=> {
-  res.sendFile(path.join(_dirname, "../frontend/testIndex.html"));
+  res.sendFile(path.join(_dirname, "../frontend/index.html"));
 });
 
 app.post("/login", (req, res) => {
