72a5143231a03f75308e57168407562dfae1c9e0
Miguel Azzopardi <miguelazz2000@gmail.com>
2022-05-01 23:08:58 +0200

    ASDASDASDAS

--- backend/routes/clean.js
+++ backend/routes/clean.js
@@ -19,7 +19,7 @@ async function listFiles() {
 
     console.log('Files:');
     files.forEach(file => {
-        if(new Date(file.metadata.timeCreated) > Date.now()-(600000)){ //3600 * 1000 * 24 == 1 day
+        if(new Date(file.metadata.timeCreated) > Date.now()-(300000)){ //3600 * 1000 * 24 == 1 day
             console.log(file.name + " is over 10 minutes old!");
         }
     });
