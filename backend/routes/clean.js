2bc62770d3d1a86111189a5d8aa70e827e8322d9
Miguel Azzopardi <miguelazz2000@gmail.com>
2022-05-01 23:05:43 +0200

    AAAAAAAAAAAAAAAAAAAAAA

--- backend/routes/clean.js
+++ backend/routes/clean.js
@@ -1,9 +1,27 @@
 import Express from "express";
-
+import { Storage } from ('@google-cloud/storage');
 const clean = Express.Router();
 
-clean.route("/").post((req,res) => {
-    console.log("Cron job works!");
+const bucketName = "pftc001.appspot.com";
+
+const storage = new Storage({
+    projectId: 'pftc001',
+    keyFilename: './key.json'
 });
 
+clean.route("/").post((req, res) => {
+    listFiles();
+});
+
+async function listFiles() {
+    // Lists files in the bucket
+    const [files] = await storage.bucket(bucketName).getFiles();
+
+    console.log('Files:');
+    files.forEach(file => {
+        if(new Date(file.getMetadata().metadata.timeCreated) > Date.now()-(600000)){ //3600 * 1000 * 24 == 1 day
+            console.log(file.name + " is over 10 minutes old!");
+        }
+    });
+}
 export default clean;
